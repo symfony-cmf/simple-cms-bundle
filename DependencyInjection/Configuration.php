fbf29dd2db2e4b558f55bfb0187dd71a17d25fb3
Lukas Kahwe Smith <smith@pooteeweet.org>
2012-10-21 17:24:57 +0200

    cosmetics

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,9 +10,8 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('symfony_cmf_simple_cms');
 
-        $rootNode
+        $treeBuilder->root('symfony_cmf_simple_cms')
             ->children()
                 ->enumNode('use_sonata_admin')
                     ->values(array(true, false, 'auto'))
@@ -30,29 +29,21 @@ class Configuration implements ConfigurationInterface
                         ->scalarNode('content_repository_id')->defaultValue('symfony_cmf_routing_extra.content_repository')->end()
                         ->arrayNode('controllers_by_class')
                             ->useAttributeAsKey('alias')
-                            ->prototype('scalar')
-                            /* why does this not work?
-                            ->prototype('array')
-                                ->children()
-                                    ->scalarNode('Symfony\Cmf\Component\Routing\RedirectRouteInterface')
-                                        ->defaultValue('symfony_cmf_routing_extra.redirect_controller:redirectAction')
-                                    ->end()
-                                ->end()
-                             */
-                        ->end()->end()
+                            ->prototype('scalar')->end()
+                        ->end()
                         ->arrayNode('templates_by_class')
                             ->useAttributeAsKey('alias')
-                            ->prototype('scalar')
-                        ->end()->end()
+                            ->prototype('scalar')->end()
+                        ->end()
                     ->end()
                 ->end()
                 ->arrayNode('multilang')
                     ->children()
                         ->arrayNode('locales')
-                            ->prototype('scalar')
-                        ->end()->end()
+                            ->prototype('scalar')->end()
+                        ->end()
                     ->end()
-                ->end()->end()
+                ->end()
             ->end()
         ;
 
