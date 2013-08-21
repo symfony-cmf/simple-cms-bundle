a62cb46b30889f213a5c373827392b16444412e3
Lukas Kahwe Smith <smith@pooteeweet.org>
2013-08-21 23:25:29 +0200

    fix case sensivity issue

--- Tests/Resources/DataFixtures/Phpcr/LoadPageData.php
+++ Tests/Resources/DataFixtures/Phpcr/LoadPageData.php
@@ -26,38 +26,39 @@ class LoadPageData implements FixtureInterface, DependentFixtureInterface
     {
         $root = $manager->find(null, '/test');
 
-        $base = new Page;
+        $base = new Page();
         $base->setName('page');
         $base->setTitle('Simple Cms');
         $base->setLabel('Simple Cms');
         $base->setParent($root);
         $manager->persist($base);
 
-        $page = new Page;
+        $page = new Page();
         $page->setName('homepage');
         $page->setTitle('Homepage');
         $page->setLabel('Homepage');
         $page->setPublishable(true);
+        $page->setAddLocalePattern(true);
         $page->setParent($base);
         $page->setBody($this->getContent('homepage.html'));
         $manager->persist($page);
 
-        $page = new Page;
+        $page = new Page();
         $page->setName('french-page');
         $page->setTitle('French Page');
         $page->setLabel('French Page');
         $page->setPublishable(true);
+        $page->setAddLocalePattern(true);
         $page->setLocale('fr');
         $page->setBody($this->getContent('french-page.html'));
         $page->setParent($base);
         $manager->persist($page);
 
-        $page = new Page;
+        $page = new Page();
         $page->setName('no-locale-prefix');
         $page->setTitle('No Locale Prefix');
         $page->setLabel('No Locale Prefix');
         $page->setPublishable(true);
-        $page->setAddLocalePattern(false);
         $page->setParent($base);
         $page->setBody($this->getContent('no-locale-prefix.html'));
         $page->setParent($base);
