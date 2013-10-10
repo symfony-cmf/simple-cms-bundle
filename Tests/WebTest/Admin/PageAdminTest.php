e90aaffaefa97c858216c783628c5070d6d67481
David Buchmann <david.buchmann@liip.ch>
2013-10-10 08:52:25 +0200

    implementing sonata admin toString

--- Tests/WebTest/Admin/PageAdminTest.php
+++ Tests/WebTest/Admin/PageAdminTest.php
@@ -45,7 +45,7 @@ class PageAdminTest extends BaseTestCase
         $crawler = $this->client->request('GET', '/admin/cmf/simplecms/page/test/page/homepage/show');
         $res = $this->client->getResponse();
         $this->assertEquals(200, $res->getStatusCode());
-        $this->assertCount(1, $crawler->filter('html:contains("homepage")'));
+        $this->assertCount(1, $crawler->filter('html:contains("Homepage")'));
     }
 
     public function testPageCreate()
